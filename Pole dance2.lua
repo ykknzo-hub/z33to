11e163d772de19c86c58c21c0f1b3a06c2448a1d
ykknzo-hub <kenzielimonn@gmail.com>
2026-03-30 00:17:33 +0100

    Update Pole dance2.lua

--- Pole dance2.lua
+++ Pole dance2.lua
@@ -1,27 +1,57 @@
 /**
- * @name Death-Note
- * @author Request
- * @version auto updates
- * @description Theme was created in generator https://vsthemes.org/generator/
- * @source https://vsthemes.org/en/skins/discord/28055-deathnote.html
- * @authorLink https://vsthemes.org/user/Request/
- * @authorId 2
+* @name Vametae
+* @version 1.0
+* @description Beautiful theme from the creator of Fanerka,swe3zy,Mellor
+* @author Fanerka,swe3zy,Mellor
+* @source https://discord.gg/PnYaEY9pFC
+* @Discord Server Where u can contact me:https://discord.gg/PnYaEY9pFC
 */
-@import url(https://vsthemes.org/uploads/generator/client/BasicBackground.css);
+@import url("https://discordstyles.github.io/FrostedGlass/dist/FrostedGlass.css");
+@import url('https://discordstyles.github.io/RadialStatus/dist/RadialStatus.css');
+@import url('https://discordstyles.github.io/HorizontalServerList/dist/HorizontalServerList.css');
 :root {
-    --transparencycolor: 0,0,0;
-    --transparencyalpha: 0.2;
-    --messagetransparency: 0.5;
-    --guildchanneltransparency: 0.2;
-    --memberlistransparency: 0;
-    --accentcolor: 54,54,54;
-    --background: url("https://vsthemes.org/uploads/posts/2021-03/1615130480_1582033425_death-note-ryuk_vsthemes_ru-1.webp");
-    --backdrop: rgba(0,0,0,0.2);
-    --backgroundblur: 0px;
-    --popoutblur: 0px;
-    --backdropblur: 0px;
-    --version1_0_5:none;
-    --backgroundsize: cover;
-    --popoutsize: cover;
-    --backdropsize: cover;
+--background-image: url('https://cdn.discordapp.com/attachments/947040293750997102/947049331746308116/2941780-japanese-digital-art-minimalism-simple-background-trees-nature-landscape-mountians-horizon-sun-monochrome-mount-fuji___landscape-nature-wallpapers.png');
+--background-image-blur: 0px;
+--background-image-size: cover;
+--background-image-position: center;
+--popout-modal-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASwAAACoCAMAAABt9SM9AAAACVBMVEUwMDAsLCwxMTGsbUDcAAABb0lEQVR4nO3QQQ0AIBDAMA7/orHAXoSkVbBszeKaWcHs1wUfMSswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzArMCswKzDr3gFT/QH17Foa0wAAAABJRU5ErkJggg==');
+--popout-modal-blur: 10px;
+--popout-modal-size: cover;
+--popout-modal-position: center;
+--home-button-image: url('https://cdn.discordapp.com/attachments/947040293750997102/947050224327737364/static_4.png');
+--home-button-size: cover;
+--home-button-position: center;
+--serverlist-brightness: 0.29;
+--left-brightness: 0.6;
+--middle-brightness: 0.29;
+--right-brightness: 0.6;
+--popout-modal-brightness: 0;
+--gradient-primary: 80,100,105; 
+--gradient-secondary: 180,207,235;
+--gradient-direction: 185deg;
+--tint-colour: 180,207,235;
+--tint-brightness: 0;
+--window-padding: 13px;
+--window-roundness: 25px;
+--scrollbar-colour: #ffffff0d;
+--link-colour: #00b0f4;
+--show-gift-gif-buttons: no-repeat;
+--font: 'Whitney';
+--update-notice-1: none;
+--HSL-server-icon-size: 40px;
+--HSL-server-spacing: 10px;
+--rs-small-spacing: 1px;
+--rs-medium-spacing: 1px;
+--rs-large-spacing: 4px;
+--rs-small-width: 2px;
+--rs-medium-width: 4px;
+--rs-large-width: 4px;
+--rs-avatar-shape: 50%;
+--rs-online-color: #43b581;
+--rs-idle-color: #faa61a;
+--rs-dnd-color: #f04747;
+--rs-offline-color: #636b75;
+--rs-streaming-color: #643da7;
+--rs-invisible-color: #747f8d;
+--rs-phone-visible: block;
 }
